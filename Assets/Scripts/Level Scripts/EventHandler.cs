a9e58ab6b460ce33fca47796c287a89977084a0e
maxembo <91334358+maxembo@users.noreply.github.com>
2023-05-06 20:37:02 +0300

    Add Menu

--- Assets/Scripts/Level Scripts/EventHandler.cs
+++ Assets/Scripts/Level Scripts/EventHandler.cs
@@ -2,6 +2,7 @@ using UnityEngine.Events;
 
 public static class EventHandler
 {
+    public static UnityEvent OnEnemyKilled = new UnityEvent();
     public static UnityEvent OnQuestPassed = new UnityEvent();
     public static UnityEvent<int> OnEvilLevelChanged = new UnityEvent<int>();
 }
---
+++ Assets/Scripts/Scenes/Buttons.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Scenes
+{
+    public class Buttons : MonoBehaviour
+    {
+        public void OnQuitPressed()
+        {
+            Application.Quit();
+        }
+    }
+}
